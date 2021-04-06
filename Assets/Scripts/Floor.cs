c9734dd35a490ded33d74cd87e5e241932d3ce02
Diego Bonetto <diego_yyz@hotmail.com>
2021-04-06 15:53:47 -0300

    floor prefab with find neigboyurs

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 public class Floor : MonoBehaviour
-{ 
+{
     public MeshRenderer Indicator;
     public Transform center;
     [SerializeField]
-    private Floor[] neighbours;
+    private List<GameObject> neighboursFinal = new List<GameObject>();
+    [SerializeField]
+    private float distance;
 
     private void Awake()
     {
-        Indicator = GetComponentInChildren<MeshRenderer>();
-        Indicator.enabled = false;            
-    }    
+        neighboursFinal = GameObject.FindGameObjectsWithTag("Floor").
+            Where(x =>x.gameObject != this.gameObject && Vector3.Distance(this.transform.position, x.transform.position) < distance).ToList();
+        Indicator.enabled = false;
+    }
     private void Walkeable()
     {
         Indicator.material.color = Color.green;
